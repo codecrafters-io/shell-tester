3fc27ae64b60a58c959c25a898095b700e811274
Ryan Gang <ryan-gg@outlook.com>
2024-05-23 13:17:34 +0530

    feat: implement test for stage-12

---
+++ internal/stage12.go
@@ -0,0 +1,32 @@
+package internal
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
+	"github.com/codecrafters-io/shell-tester/internal/test_cases"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+func testCd3(stageHarness *test_case_harness.TestCaseHarness) error {
+	logger := stageHarness.Logger
+	shell := shell_executable.NewShellExecutable(stageHarness)
+
+	if err := shell.Start(); err != nil {
+		return err
+	}
+
+	testCase1 := test_cases.CDAndPWDTestCase{Directory: "/usr/local/bin", Response: "/usr/local/bin"}
+	testCase1.Run(shell, logger)
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("CodeCrafters internal error. Error getting home directory: %v", err)
+	}
+
+	testCase2 := test_cases.CDAndPWDTestCase{Directory: "~", Response: homeDir}
+	testCase2.Run(shell, logger)
+
+	return promptCheck(shell, logger)
+}
