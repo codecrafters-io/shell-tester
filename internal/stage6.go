ba0a942198c21c526cafecbcbc15d7ddebbbed6b
Ryan Gang <ryan-gg@outlook.com>
2024-11-21 22:05:47 +0530

    refactor: usage of SingleLineOutputTestCase in stages

--- internal/stage6.go
+++ internal/stage6.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"fmt"
-	"regexp"
 
 	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
 	"github.com/codecrafters-io/shell-tester/internal/test_cases"
@@ -22,10 +21,10 @@ func testType1(stageHarness *test_case_harness.TestCaseHarness) error {
 	for _, builtIn := range builtIns {
 		command := fmt.Sprintf("type %s", builtIn)
 
-		testCase := test_cases.SingleLineOutputTestCase{
+		testCase := test_cases.SingleLineExactMatchTestCase{
 			Command:                    command,
-			ExpectedPattern:            regexp.MustCompile(fmt.Sprintf(`^%s is a( special)? shell builtin$`, builtIn)),
-			ExpectedPatternExplanation: fmt.Sprintf("match %q", fmt.Sprintf(`%s is a shell builtin`, builtIn)),
+			ExpectedPattern:            fmt.Sprintf(`^%s is a( special)? shell builtin$`, builtIn),
+			ExpectedPatternExplanation: fmt.Sprintf(`%s is a shell builtin`, builtIn),
 			SuccessMessage:             "Received expected response",
 		}
 		if err := testCase.Run(shell, logger); err != nil {
@@ -38,10 +37,10 @@ func testType1(stageHarness *test_case_harness.TestCaseHarness) error {
 	for _, invalidCommand := range invalidCommands {
 		command := fmt.Sprintf("type %s", invalidCommand)
 
-		testCase := test_cases.SingleLineOutputTestCase{
+		testCase := test_cases.SingleLineExactMatchTestCase{
 			Command:                    command,
-			ExpectedPattern:            regexp.MustCompile(fmt.Sprintf(`^(bash: type: )?%s[:]? not found$`, invalidCommand)),
-			ExpectedPatternExplanation: fmt.Sprintf("contain %q", fmt.Sprintf(`%s: not found`, invalidCommand)),
+			ExpectedPattern:            fmt.Sprintf(`^(bash: type: )?%s[:]? not found$`, invalidCommand),
+			ExpectedPatternExplanation: fmt.Sprintf("%s: not found", invalidCommand),
 			SuccessMessage:             "Received expected response",
 		}
 		if err := testCase.Run(shell, logger); err != nil {
