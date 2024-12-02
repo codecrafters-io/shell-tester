1a60a73ec6abc87360e95598b8a306fb1f4e5fb8
Ryan Gang <ryan-gg@outlook.com>
2024-12-02 14:27:37 +0530

    fix: update command execution in testpwd function to properly format shell command for compatibility

--- internal/stage9.go
+++ internal/stage9.go
@@ -47,7 +47,7 @@ func testpwd(stageHarness *test_case_harness.TestCaseHarness) error {
 	if pwdNotFoundErr == nil && runtime.GOOS != "darwin" {
 		// os.Rename is unable to complete this operation on some systems due to permission issues
 		command := fmt.Sprintf("%s %s %s", moveCommand, path, newPath)
-		cmd := exec.Command("sh", "-c", command)
+		cmd := exec.Command("sh", "-c", fmt.Sprintf("'%s'", command))
 		err = cmd.Run()
 		if err != nil {
 			logger.Errorf(cmd.String())
