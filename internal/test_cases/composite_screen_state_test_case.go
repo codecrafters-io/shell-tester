b3896f5646976965677a3dec31fff0f8ce4f9bf6
Ryan Gang <ryan-gg@outlook.com>
2024-12-06 13:44:11 +0530

    feat: implement stack-based CompositeScreenStateTestCase with assertion management methods

--- internal/test_cases/composite_screen_state_test_case.go
+++ internal/test_cases/composite_screen_state_test_case.go
@@ -1,7 +1,53 @@
 package test_cases
 
-import "github.com/codecrafters-io/shell-tester/internal/assertions"
+import (
+	"github.com/codecrafters-io/shell-tester/internal/assertions"
+	"github.com/codecrafters-io/tester-utils/logger"
+)
 
+// Stack-based implementation of CompositeScreenStateTestCase
+// This is a stack of assertions that will be run in order
+// It's a stack because we want to be able to push and pop assertions
 type CompositeScreenStateTestCase struct {
 	ScreenStateAssertions []assertions.Assertion
+}
+
+func NewCompositeScreenStateTestCase() *CompositeScreenStateTestCase {
+	return &CompositeScreenStateTestCase{ScreenStateAssertions: []assertions.Assertion{}}
+}
+
+func (t *CompositeScreenStateTestCase) Run(screenState [][]string, logger *logger.Logger) error {
+	for _, assertion := range t.ScreenStateAssertions {
+		if err := assertion.Run(screenState, logger); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (t *CompositeScreenStateTestCase) PushAssertion(assertion assertions.Assertion) {
+	t.ScreenStateAssertions = append(t.ScreenStateAssertions, assertion)
+}
+
+func (t *CompositeScreenStateTestCase) PopAssertion() assertions.Assertion {
+	if len(t.ScreenStateAssertions) == 0 {
+		panic("CodeCrafters Internal Error: no assertions to pop")
+	}
+
+	assertion := t.ScreenStateAssertions[len(t.ScreenStateAssertions)-1]
+	t.ScreenStateAssertions = t.ScreenStateAssertions[:len(t.ScreenStateAssertions)-1]
+	return assertion
+}
+
+func (t *CompositeScreenStateTestCase) PeekAssertion() assertions.Assertion {
+	if len(t.ScreenStateAssertions) == 0 {
+		panic("CodeCrafters Internal Error: no assertions to peek")
+	}
+
+	return t.ScreenStateAssertions[len(t.ScreenStateAssertions)-1]
+}
+
+func (t *CompositeScreenStateTestCase) AssertionsCount() int {
+	return len(t.ScreenStateAssertions)
 }
