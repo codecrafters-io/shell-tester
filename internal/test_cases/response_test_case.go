f753758b8f15db37eb297121a2db9d521f5b2a64
Ryan Gang <ryan-gg@outlook.com>
2024-12-06 17:24:18 +0530

    draft: implement response test case

--- internal/test_cases/response_test_case.go
+++ internal/test_cases/response_test_case.go
@@ -1,30 +1,51 @@
 package test_cases
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/codecrafters-io/shell-tester/internal/assertions"
-	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
-	"github.com/codecrafters-io/tester-utils/logger"
 )
 
+// ToDo: This is a prototype, think about edge cases + implement prompt test case specifically
 // TODO: Remove ResponseTestCase entirely, replace with SingleLineOutputAssertion invoked within ScreenAsserter
 // ResponseTestCase reads the output from the shell, and verifies that it matches the expected output.
 type ResponseTestCase struct {
-	// expectedPrompt is the prompt expected to be displayed (example: "$ ")
-	expectedPrompt string
+	// command is the command that will be sent to the shell
+	command string
 
-	assertion assertions.SingleLineScreenStateAssertion
+	assertion assertions.Assertion
 
 	shouldOmitSuccessLog bool
 }
 
-func NewResponseTestCase(expectedPrompt string, assertion assertions.SingleLineScreenStateAssertion, shouldOmitSuccessLog bool) ResponseTestCase {
-	return ResponseTestCase{expectedPrompt: expectedPrompt, assertion: assertion, shouldOmitSuccessLog: shouldOmitSuccessLog}
+func NewResponseTestCase(command string, assertion assertions.Assertion, shouldOmitSuccessLog bool) ResponseTestCase {
+	return ResponseTestCase{command: command, assertion: assertion, shouldOmitSuccessLog: shouldOmitSuccessLog}
 }
 
-func NewSilentResponseTestCase(expectedPrompt string, assertion assertions.SingleLineScreenStateAssertion) ResponseTestCase {
-	return ResponseTestCase{expectedPrompt: expectedPrompt, assertion: assertion, shouldOmitSuccessLog: true}
-}
+func (t ResponseTestCase) Run(screenAsserter assertions.ScreenAsserter) error {
+	err := screenAsserter.Shell.ReadUntil(t.assertion.WrappedRun)
 
-func (t ResponseTestCase) Run(shell *shell_executable.ShellExecutable, logger *logger.Logger) error {
+	if err != nil {
+		// If the user sent any output, let's print it before the error message.
+		if len(screenAsserter.Shell.GetScreenState()) > 0 {
+			screenAsserter.LogFullScreenState()
+		}
+
+		return fmt.Errorf("Expected prompt (%q) to be printed, got %q", t.command, screenAsserter.Shell.GetScreenState()[0])
+	}
+
+	err = screenAsserter.Shell.ReadUntilTimeout(10 * time.Millisecond)
+
+	// Whether the value matches our expectations or not, we print it
+	screenAsserter.LogFullScreenState()
+
+	// We failed to read extra output
+	if err != nil {
+		return fmt.Errorf("Error reading output: %v", err)
+	}
+
+	screenAsserter.Logger.Successf("✓ Received prompt")
+
 	return nil
 }
