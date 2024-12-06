c281fa79b16cfb7adc97d4eb988162d847f1a6f3
Ryan Gang <ryan-gg@outlook.com>
2024-12-06 17:22:53 +0530

    feat: enhance ScreenAsserter with constructor and logging functionality

--- internal/assertions/screen_asserter.go
+++ internal/assertions/screen_asserter.go
@@ -1,12 +1,24 @@
 package assertions
 
 import (
+	"strings"
+
 	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
 	"github.com/codecrafters-io/tester-utils/logger"
 )
 
 // ToDo: Prototype, not yet sure what the ideal consituents of this struct should be
 type ScreenAsserter struct {
-	shell  *shell_executable.ShellExecutable
-	logger *logger.Logger
+	Shell  *shell_executable.ShellExecutable
+	Logger *logger.Logger
+}
+
+func NewScreenAsserter(shell *shell_executable.ShellExecutable, logger *logger.Logger) ScreenAsserter {
+	return ScreenAsserter{Shell: shell, Logger: logger}
+}
+
+func (s ScreenAsserter) LogFullScreenState() {
+	for _, row := range s.Shell.GetScreenState() {
+		s.Logger.Debugf(strings.Join(row, ""))
+	}
 }
