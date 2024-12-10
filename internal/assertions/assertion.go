321f0e2aa693958f1c75474562928d50d39727bd
Ryan Gang <ryan-gg@outlook.com>
2024-12-10 12:15:22 +0530

    feat: introduce BaseAssertion struct to standardize assertion row handling and state management

--- internal/assertions/assertion.go
+++ internal/assertions/assertion.go
@@ -1,5 +1,18 @@
 package assertions
 
+// BaseAssertion contains elements mandatory to all assertions.
+type BaseAssertion struct {
+	// screenAsserter is the ScreenAsserter that contains the rendered screenstate
+	screenAsserter *ScreenAsserter
+
+	// rowIndex is the index of the row in the screenstate that we want to assert on
+	rowIndex int
+
+	// Each assertion should update the row index in screenAsserter atmost once
+	// Once it performs the update, it should set this flag to true
+	ifUpdatedRowIndex bool
+}
+
 type Assertion interface {
 	Run() error
 	WrappedRun() bool
