718f34be67b696b324aae733460a62b59144bdcf
Ryan Gang <ryan-gg@outlook.com>
2024-12-11 15:07:19 +0530

    refactor: replace Run and ReadUntil methods with RunWithPromptAssertion and add a new AsBool helper

--- internal/stage2.go
+++ internal/stage2.go
@@ -15,27 +15,25 @@ func testMissingCommand(stageHarness *test_case_harness.TestCaseHarness) error {
 		return err
 	}
 
-	screenAsserter := assertions.NewScreenAsserter(shell, logger).WithPromptAssertion()
-	if err := screenAsserter.Shell.ReadUntil(screenAsserter.RunBool); err != nil {
+	screenAsserter := assertions.NewScreenAsserter(shell, logger)
+	if err := screenAsserter.Shell.ReadUntil(AsBool(screenAsserter.RunWithPromptAssertion)); err != nil {
 		return err
 	}
 
 	// Checks if prompt is present
-	if err := screenAsserter.Run(); err != nil {
+	if err := screenAsserter.RunWithPromptAssertion(); err != nil {
 		return err
 	}
-	screenAsserter.PopAssertion()
 
 	// TODO: Can shorten into a SingleLineCommandTestCase
 	// ------ Test case starts
 	shell.SendCommand("nonexistent")
 	screenAsserter.PushAssertion(screenAsserter.SingleLineAssertion(0, "$ nonexistent", nil, "nonexistent"))
 	screenAsserter.PushAssertion(screenAsserter.SingleLineAssertion(1, "", []*regexp.Regexp{regexp.MustCompile(`^bash: nonexistent: command not found$`)}, "bash: nonexistent: command not found"))
-	screenAsserter.PushAssertion(screenAsserter.PromptAssertion("$ "))
-	if err := screenAsserter.Shell.ReadUntil(screenAsserter.RunBool); err != nil {
+	if err := screenAsserter.Shell.ReadUntil(AsBool(screenAsserter.RunWithPromptAssertion)); err != nil {
 		return err
 	}
-	if err := screenAsserter.Run(); err != nil {
+	if err := screenAsserter.RunWithPromptAssertion(); err != nil {
 		return err
 	}
 	logger.Successf("$ ")
@@ -87,4 +85,11 @@ func testMissingCommand(stageHarness *test_case_harness.TestCaseHarness) error {
 	// }
 
 	return nil
+}
+
+func AsBool(T func() error) func() bool {
+	// Takes in a function that takes no params & returns an error
+	// Returns the function wrapped in a helper such that it returns a bool
+	// in liue of the error, true if the function execution is a success
+	return func() bool { return T() == nil }
 }
