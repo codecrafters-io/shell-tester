8596eeb87bb9d8cdc85878439a482dbbc7c4ebca
Ryan Gang <ryan-gg@outlook.com>
2024-12-12 19:20:11 +0530

    refactor: enhance CommandResponseTestCase structure

--- internal/test_cases/command_response_test_case.go
+++ internal/test_cases/command_response_test_case.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"regexp"
 
-	"github.com/codecrafters-io/shell-tester/internal/screen_asserter"
+	"github.com/codecrafters-io/shell-tester/internal/assertions"
+	"github.com/codecrafters-io/shell-tester/internal/logged_shell_asserter"
+	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
+	"github.com/codecrafters-io/tester-utils/logger"
 )
 
 // CommandResponseTestCase
@@ -13,38 +16,37 @@ import (
 // Reads the output from the shell, and verifies that it matches the expected output
 // If any error occurs returns the error from the corresponding assertion
 type CommandResponseTestCase struct {
-	command string
+	// Command is the command to send to the shell
+	Command string
 
-	// expectedOutput is the expected output string to match against
-	expectedOutput string
+	// ExpectedOutput is the expected output string to match against
+	ExpectedOutput string
 
-	// fallbackPatterns is a list of regex patterns to match against
-	fallbackPatterns []*regexp.Regexp
+	// FallbackPatterns is a list of regex patterns to match against
+	FallbackPatterns []*regexp.Regexp
 
-	// expectedPatternExplanation is the explanation of the expected pattern to
-	// show in the error message in case of failure
-	expectedPatternExplanation string
+	// SuccessMessage is the message to log in case of success
+	SuccessMessage string
 }
 
-func NewCommandResponseTestCase(command string, expectedOutput string, fallbackPatterns []*regexp.Regexp, expectedPatternExplanation string) CommandResponseTestCase {
-	return CommandResponseTestCase{command: command, expectedOutput: expectedOutput, fallbackPatterns: fallbackPatterns, expectedPatternExplanation: expectedPatternExplanation}
-}
-
-func (t CommandResponseTestCase) Run(screenAsserter *screen_asserter.ScreenAsserter) error {
-	err := screenAsserter.Shell.SendCommand(t.command)
-	if err != nil {
-		return fmt.Errorf("Error sending command: %v", err)
+func (t CommandResponseTestCase) Run(shell *shell_executable.ShellExecutable, logger *logger.Logger, asserter *logged_shell_asserter.LoggedShellAsserter) error {
+	if err := shell.SendCommand(t.Command); err != nil {
+		return fmt.Errorf("Error sending command to shell: %v", err)
 	}
 
-	// expectedCommandLine := fmt.Sprintf("$ %s", t.command)
-	// screenAsserter.PushAssertion(assertions.NewSingleLineScreenStateAssertion(expectedCommandLine, nil, ""))
-	// screenAsserter.PushAssertion(assertions.NewSingleLineScreenStateAssertion(t.expectedOutput, t.fallbackPatterns, t.expectedPatternExplanation))
+	commandReflection := fmt.Sprintf("$ %s", t.Command)
+	asserter.AddAssertion(assertions.SingleLineAssertion{
+		ExpectedOutput: commandReflection,
+	})
+	asserter.AddAssertion(assertions.SingleLineAssertion{
+		ExpectedOutput:   t.ExpectedOutput,
+		FallbackPatterns: t.FallbackPatterns,
+	})
 
-	// if err := screenAsserter.Shell.ReadUntil(utils.AsBool(screenAsserter.RunWithPromptAssertion)); err != nil {
-	// 	if err := screenAsserter.RunWithPromptAssertion(); err != nil {
-	// 		return err
-	// 	}
-	// }
+	if err := asserter.Assert(); err != nil {
+		return err
+	}
 
+	logger.Successf(t.SuccessMessage)
 	return nil
 }
