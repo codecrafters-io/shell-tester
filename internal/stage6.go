cdabff6e1e7443811512f2a7d865badcfea3a51e
Ryan Gang <ryan-gg@outlook.com>
2024-12-12 20:06:57 +0530

    refactor: integrate logged shell asserter into stage4 and stage6 tests, update CommandResponseTestCase execution

--- internal/stage6.go
+++ internal/stage6.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 
+	"github.com/codecrafters-io/shell-tester/internal/logged_shell_asserter"
 	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
 	"github.com/codecrafters-io/shell-tester/internal/test_cases"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
@@ -12,6 +13,7 @@ import (
 func testType1(stageHarness *test_case_harness.TestCaseHarness) error {
 	logger := stageHarness.Logger
 	shell := shell_executable.NewShellExecutable(stageHarness)
+	asserter := logged_shell_asserter.NewLoggedShellAsserter(shell)
 
 	builtIns := []string{"echo", "exit", "type"}
 
@@ -19,16 +21,21 @@ func testType1(stageHarness *test_case_harness.TestCaseHarness) error {
 		return err
 	}
 
+	// First prompt assertion
+	if err := asserter.Assert(); err != nil {
+		return err
+	}
+
 	for _, builtIn := range builtIns {
 		command := fmt.Sprintf("type %s", builtIn)
 
-		testCase := test_cases.SingleLineExactMatchTestCase{
-			Command:                    command,
-			FallbackPatterns:           []*regexp.Regexp{regexp.MustCompile(fmt.Sprintf(`^%s is a( special)? shell builtin$`, builtIn))},
-			ExpectedPatternExplanation: fmt.Sprintf(`%s is a shell builtin`, builtIn),
-			SuccessMessage:             "Received expected response",
+		testCase := test_cases.CommandResponseTestCase{
+			Command:          command,
+			ExpectedOutput:   fmt.Sprintf(`%s is a shell builtin`, builtIn),
+			FallbackPatterns: []*regexp.Regexp{regexp.MustCompile(fmt.Sprintf(`^%s is a( special)? shell builtin$`, builtIn))},
+			SuccessMessage:   "Received expected response",
 		}
-		if err := testCase.Run(shell, logger); err != nil {
+		if err := testCase.Run(asserter, shell, logger); err != nil {
 			return err
 		}
 	}
@@ -38,13 +45,13 @@ func testType1(stageHarness *test_case_harness.TestCaseHarness) error {
 	for _, invalidCommand := range invalidCommands {
 		command := fmt.Sprintf("type %s", invalidCommand)
 
-		testCase := test_cases.SingleLineExactMatchTestCase{
-			Command:                    command,
-			FallbackPatterns:           []*regexp.Regexp{regexp.MustCompile(fmt.Sprintf(`^(bash: type: )?%s[:]? not found$`, invalidCommand))},
-			ExpectedPatternExplanation: fmt.Sprintf("%s: not found", invalidCommand),
-			SuccessMessage:             "Received expected response",
+		testCase := test_cases.CommandResponseTestCase{
+			Command:          command,
+			ExpectedOutput:   fmt.Sprintf("%s: not found", invalidCommand),
+			FallbackPatterns: []*regexp.Regexp{regexp.MustCompile(fmt.Sprintf(`^(bash: type: )?%s[:]? not found$`, invalidCommand))},
+			SuccessMessage:   "Received expected response",
 		}
-		if err := testCase.Run(shell, logger); err != nil {
+		if err := testCase.Run(asserter, shell, logger); err != nil {
 			return err
 		}
 	}
