b0cbf9d2e90ab4ee7125575bc3a2080e17fe5688
Ryan Gang <ryan-gg@outlook.com>
2024-12-12 20:28:01 +0530

    refactor: integrate logged shell asserter into stage7 tests and update CommandResponseTestCase execution

--- internal/stage7.go
+++ internal/stage7.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 
 	"github.com/codecrafters-io/shell-tester/internal/custom_executable"
+	"github.com/codecrafters-io/shell-tester/internal/logged_shell_asserter"
 	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
 	"github.com/codecrafters-io/shell-tester/internal/test_cases"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
@@ -25,6 +26,7 @@ func testType2(stageHarness *test_case_harness.TestCaseHarness) error {
 	logger := stageHarness.Logger
 	shell := shell_executable.NewShellExecutable(stageHarness)
 	shell.Setenv("PATH", fmt.Sprintf("%s:%s", randomDir, path))
+	asserter := logged_shell_asserter.NewLoggedShellAsserter(shell)
 
 	customExecutablePath := filepath.Join(randomDir, "my_exe")
 	err = custom_executable.CreateExecutable(getRandomString(), customExecutablePath)
@@ -33,6 +35,11 @@ func testType2(stageHarness *test_case_harness.TestCaseHarness) error {
 	}
 
 	if err := shell.Start(); err != nil {
+		return err
+	}
+
+	// First prompt assertion
+	if err := asserter.Assert(); err != nil {
 		return err
 	}
 
@@ -53,13 +60,13 @@ func testType2(stageHarness *test_case_harness.TestCaseHarness) error {
 			expectedPath = path
 		}
 
-		testCase := test_cases.SingleLineExactMatchTestCase{
-			Command:                    command,
-			FallbackPatterns:           []*regexp.Regexp{regexp.MustCompile(fmt.Sprintf(`^(%s is )?%s$`, executable, expectedPath))},
-			ExpectedPatternExplanation: fmt.Sprintf(`%s is %s`, executable, expectedPath),
-			SuccessMessage:             "Received expected response",
+		testCase := test_cases.CommandResponseTestCase{
+			Command:          command,
+			ExpectedOutput:   fmt.Sprintf(`%s is %s`, executable, expectedPath),
+			FallbackPatterns: []*regexp.Regexp{regexp.MustCompile(fmt.Sprintf(`^(%s is )?%s$`, executable, expectedPath))},
+			SuccessMessage:   "Received expected response",
 		}
-		if err := testCase.Run(shell, logger); err != nil {
+		if err := testCase.Run(asserter, shell, logger); err != nil {
 			return err
 		}
 	}
@@ -68,13 +75,13 @@ func testType2(stageHarness *test_case_harness.TestCaseHarness) error {
 
 	for _, executable := range nonAvailableExecutables {
 		command := fmt.Sprintf("type %s", executable)
-		testCase := test_cases.SingleLineExactMatchTestCase{
-			Command:                    command,
-			FallbackPatterns:           []*regexp.Regexp{regexp.MustCompile(fmt.Sprintf(`^(bash: type: )?%s: not found$`, executable))},
-			ExpectedPatternExplanation: fmt.Sprintf(`%s: not found`, executable),
-			SuccessMessage:             "Received expected response",
+		testCase := test_cases.CommandResponseTestCase{
+			Command:          command,
+			ExpectedOutput:   fmt.Sprintf(`%s: not found`, executable),
+			FallbackPatterns: []*regexp.Regexp{regexp.MustCompile(fmt.Sprintf(`^(bash: type: )?%s: not found$`, executable))},
+			SuccessMessage:   "Received expected response",
 		}
-		if err := testCase.Run(shell, logger); err != nil {
+		if err := testCase.Run(asserter, shell, logger); err != nil {
 			return err
 		}
 	}
