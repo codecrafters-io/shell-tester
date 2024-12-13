e8d6cdd8c8f1cd9a3e53ed8a1d1dcdd57a581279
Ryan Gang <ryan-gg@outlook.com>
2024-12-13 19:38:57 +0530

    fix: remove checking for final prompt in stage2

--- internal/stage2.go
+++ internal/stage2.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"regexp"
 
+	"github.com/codecrafters-io/shell-tester/internal/assertions"
 	"github.com/codecrafters-io/shell-tester/internal/logged_shell_asserter"
 	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
-	"github.com/codecrafters-io/shell-tester/internal/test_cases"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
@@ -21,19 +21,27 @@ func testMissingCommand(stageHarness *test_case_harness.TestCaseHarness) error {
 
 	invalidCommand := "nonexistent"
 
-	test_case := test_cases.CommandResponseTestCase{
-		Command:        invalidCommand,
+	if err := shell.SendCommand(invalidCommand); err != nil {
+		return fmt.Errorf("Error sending command to shell: %v", err)
+	}
+
+	commandReflection := fmt.Sprintf("$ %s", invalidCommand)
+	asserter.AddAssertion(assertions.SingleLineAssertion{
+		ExpectedOutput: commandReflection,
+	})
+	asserter.AddAssertion(assertions.SingleLineAssertion{
 		ExpectedOutput: fmt.Sprintf("%s: command not found", invalidCommand),
 		FallbackPatterns: []*regexp.Regexp{
 			regexp.MustCompile(fmt.Sprintf(`^bash: %s: command not found$`, invalidCommand)),
 			regexp.MustCompile(fmt.Sprintf(`^%s: command not found$`, invalidCommand)),
 		},
-		SuccessMessage: "✓ Received command not found message",
-	}
+	})
 
-	if err := test_case.Run(asserter, shell, logger); err != nil {
+	if err := asserter.AssertWithPrompt(); err != nil {
 		return err
 	}
+
+	logger.Successf("✓ Received command not found message")
 
 	return logAndQuit(asserter, nil)
 }
