918e82767ef5a9a63c7ca476c2274b83162df560
Ryan Gang <ryan-gg@outlook.com>
2024-12-20 13:18:07 +0530

    refactor: enhance testR4 function by consolidating command test cases and improving random word generation
    This commit refactors the `testR4` function in `stage_r4.go` by consolidating variable assignments and enhancing the structure of command test cases. Key changes include using tuple assignment for `stageDir` and `lsDir`, updating the random word generation method to `RandomElementsFromArray`, and streamlining command definitions. These improvements enhance code readability while maintaining the functionality of command execution and output validation.

--- internal/stage_r4.go
+++ internal/stage_r4.go
@@ -26,13 +26,11 @@ func testR4(stageHarness *test_case_harness.TestCaseHarness) error {
 	if err != nil {
 		return err
 	}
-	stageDir := dirs[0]
-	lsDir := dirs[1]
+	stageDir, lsDir := dirs[0], dirs[1]
 	defer cleanupDirectories(dirs)
 
 	randomWords := random.RandomWords(3)
 	slices.Sort(randomWords)
-
 	filePaths := []string{
 		path.Join(lsDir, fmt.Sprintf("%s", randomWords[0])),
 		path.Join(lsDir, fmt.Sprintf("%s", randomWords[1])),
@@ -47,96 +45,98 @@ func testR4(stageHarness *test_case_harness.TestCaseHarness) error {
 		return err
 	}
 
-	randomWords2 := random.RandomWords(3)
+	randomWords2 := random.RandomElementsFromArray(SMALL_WORDS, 3)
 	slices.Sort(randomWords2)
-	outputFilePath := path.Join(stageDir, randomWords2[0]+".md")
+	outputFilePath1 := path.Join(stageDir, randomWords2[0]+".md")
 	outputFilePath2 := path.Join(stageDir, randomWords2[1]+".md")
 	outputFilePath3 := path.Join(stageDir, randomWords2[2]+".md")
 
-	command1 := fmt.Sprintf("ls %s >> %s", "nonexistent", outputFilePath)
+	// Test1:
+	// ls nonexistent >> tmp.md
 
-	reflectionTestCase := test_cases.CommandResponseTestCase{
+	command1 := fmt.Sprintf("ls %s >> %s", "nonexistent", outputFilePath1)
+
+	responseTestCase := test_cases.CommandResponseTestCase{
 		Command:          command1,
 		ExpectedOutput:   fmt.Sprintf("ls: %s: No such file or directory", "nonexistent"),
 		FallbackPatterns: nil,
 		SuccessMessage:   "✓ Received error message",
 	}
-
 	asserter.AddAssertion(assertions.FileContentAssertion{
-		FilePath:        outputFilePath,
+		FilePath:        outputFilePath1,
 		ExpectedContent: "",
 	})
-
-	if err := reflectionTestCase.Run(asserter, shell, logger); err != nil {
+	if err := responseTestCase.Run(asserter, shell, logger); err != nil {
 		return err
 	}
+	logger.Successf("✓ File: %s is empty", outputFilePath1)
 
-	command3 := fmt.Sprintf("ls %s 2>> %s", "nonexistent", outputFilePath2)
-	command4 := fmt.Sprintf("cat %s", outputFilePath2)
+	// Test2:
+	// ls nonexistent 2>> tmp.md
 
-	reflectionTestCase2 := test_cases.CommandReflectionTestCase{
-		Command: command3,
-	}
-	if err := reflectionTestCase2.Run(asserter, shell, logger, true); err != nil {
+	command2 := fmt.Sprintf("ls %s 2>> %s", "nonexistent", outputFilePath2)
+	command3 := fmt.Sprintf("cat %s", outputFilePath2)
+
+	err = test_cases.CommandReflectionTestCase{
+		Command: command2,
+	}.Run(asserter, shell, logger, true)
+	if err != nil {
 		return err
 	}
-
-	responseTestCase := test_cases.CommandResponseTestCase{
-		Command:          command4,
+	responseTestCase = test_cases.CommandResponseTestCase{
+		Command:          command3,
 		ExpectedOutput:   fmt.Sprintf("ls: %s: No such file or directory", "nonexistent"),
 		FallbackPatterns: nil,
 		SuccessMessage:   "✓ Received redirected file content",
 	}
-
 	if err := responseTestCase.Run(asserter, shell, logger); err != nil {
 		return err
 	}
 
-	// //////
+	// Test3:
+	// echo "Error" 2>> tmp.md
+	// cat nonexistent 2>> tmp.md
+	// ls nonexistent 2>> tmp.md
+	// cat tmp.md
 
 	message := fmt.Sprintf("%s says Error", getRandomName())
-	command5_1 := fmt.Sprintf(`echo "%s" 2>> %s`, message, outputFilePath3)
-	command5_2 := fmt.Sprintf(`cat %s 2>> %s`, "nonexistent", outputFilePath3)
-	command5_3 := fmt.Sprintf("ls %s 2>> %s", "nonexistent", outputFilePath3)
-	command5_4 := fmt.Sprintf("cat %s", outputFilePath3)
+	command4 := fmt.Sprintf(`echo "%s" 2>> %s`, message, outputFilePath3)
+	command5 := fmt.Sprintf(`cat %s 2>> %s`, "nonexistent", outputFilePath3)
+	command6 := fmt.Sprintf("ls %s 2>> %s", "nonexistent", outputFilePath3)
+	command7 := fmt.Sprintf("cat %s", outputFilePath3)
 
-	responseTestCase5_1 := test_cases.CommandResponseTestCase{
-		Command:          command5_1,
+	responseTestCase = test_cases.CommandResponseTestCase{
+		Command:          command4,
 		ExpectedOutput:   message,
 		FallbackPatterns: nil,
 		SuccessMessage:   "✓ Received redirected file content",
 	}
-
-	if err := responseTestCase5_1.Run(asserter, shell, logger); err != nil {
+	if err := responseTestCase.Run(asserter, shell, logger); err != nil {
 		return err
 	}
-
-	reflectionTestCase5_2 := test_cases.CommandReflectionTestCase{
-		Command: command5_2,
-	}
-	if err := reflectionTestCase5_2.Run(asserter, shell, logger, true); err != nil {
+	err = test_cases.CommandReflectionTestCase{
+		Command: command5,
+	}.Run(asserter, shell, logger, true)
+	if err != nil {
 		return err
 	}
-
-	reflectionTestCase5_3 := test_cases.CommandReflectionTestCase{
-		Command: command5_3,
-	}
-	if err := reflectionTestCase5_3.Run(asserter, shell, logger, true); err != nil {
+	err = test_cases.CommandReflectionTestCase{
+		Command: command6,
+	}.Run(asserter, shell, logger, true)
+	if err != nil {
 		return err
 	}
-
 	errorMessagesInFile := []string{
 		"cat: nonexistent: No such file or directory",
 		"ls: nonexistent: No such file or directory",
 	}
-	responseTestCase5_4 := test_cases.CommandWithMultilineResponseTestCase{
-		Command:          command5_4,
+	multiLineResponseTestCase := test_cases.CommandWithMultilineResponseTestCase{
+		Command:          command7,
 		ExpectedOutput:   errorMessagesInFile,
 		FallbackPatterns: nil,
 		SuccessMessage:   "✓ Received redirected file content",
 	}
-
-	if err := responseTestCase5_4.Run(asserter, shell, logger); err != nil {
+	if err := multiLineResponseTestCase.Run(asserter, shell, logger); err != nil {
 		return err
 	}
 
