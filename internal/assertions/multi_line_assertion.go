b0145ed7cc3a1872ee5aa6380e52a421a42aa68b
Ryan Gang <ryan-gg@outlook.com>
2024-12-27 21:28:07 +0530

    refactor: enhance MultiLineAssertion structure and methods
    - Introduced SingleLineAssertions to allow for more granular assertions.
    - Added NewMultiLineAssertion and NewEmptyMultiLineAssertion constructors.
    - Implemented AddSingleLineAssertion method for adding single line assertions with fallback patterns.
    - Updated Inspect and Run methods to accommodate the new structure.

--- internal/assertions/multi_line_assertion.go
+++ internal/assertions/multi_line_assertion.go
@@ -3,54 +3,59 @@ package assertions
 import (
 	"fmt"
 	"regexp"
-	"strings"
-
-	"github.com/codecrafters-io/shell-tester/internal/utils"
-	virtual_terminal "github.com/codecrafters-io/shell-tester/internal/vt"
 )
 
 // MultiLineAssertion asserts that multiple lines of output matches against a given array of strings
 // Or a multi-line regex pattern(s)
 type MultiLineAssertion struct {
-	// ExpectedOutput is the array of expected output strings to match against
-	ExpectedOutput []string
-
-	// FallbackPatterns is a list of regex patterns to match against. This is useful to handle shell-specific variable behaviour
-	FallbackPatterns []*regexp.Regexp
+	SingleLineAssertions []SingleLineAssertion
 }
 
-func (a MultiLineAssertion) Inspect() string {
-	return fmt.Sprintf("MultiLineAssertion (%q)", a.ExpectedOutput)
+func NewMultiLineAssertion(expectedOutput []string) MultiLineAssertion {
+	// No way to add fallbackPatterns through this constructor
+	singleLineAssertions := []SingleLineAssertion{}
+	for _, expectedLine := range expectedOutput {
+		singleLineAssertions = append(singleLineAssertions, SingleLineAssertion{
+			ExpectedOutput: expectedLine,
+		})
+	}
+
+	fmt.Println(singleLineAssertions)
+
+	return MultiLineAssertion{
+		SingleLineAssertions: singleLineAssertions,
+	}
 }
 
-func (a MultiLineAssertion) Run(screenState [][]string, startRowIndex int) (processedRowCount int, err *AssertionError) {
-	if len(a.ExpectedOutput) == 0 {
-		panic("CodeCrafters Internal Error: ExpectedOutput must be provided")
+func NewEmptyMultiLineAssertion() MultiLineAssertion {
+	return MultiLineAssertion{
+		SingleLineAssertions: []SingleLineAssertion{},
 	}
+}
 
-	totalRows := len(a.ExpectedOutput)
-	rawRows := screenState[startRowIndex : startRowIndex+totalRows]
-	cleanedRows := []string{}
-	for _, rawRow := range rawRows {
-		cleanedRows = append(cleanedRows, virtual_terminal.BuildCleanedRow(rawRow))
-	}
-	cleanedRowsString := strings.Join(cleanedRows, "\n")
-	expectedOutputString := strings.Join(a.ExpectedOutput, "\n")
+// AddSingleLineAssertion is the recommended way to add single line assertions
+// When they contain fallbackPatterns
+func (a *MultiLineAssertion) AddSingleLineAssertion(expectedOutput string, fallbackPatterns []*regexp.Regexp) *MultiLineAssertion {
+	a.SingleLineAssertions = append(a.SingleLineAssertions, SingleLineAssertion{
+		ExpectedOutput:   expectedOutput,
+		FallbackPatterns: fallbackPatterns,
+	})
+	return a
+}
 
-	for _, pattern := range a.FallbackPatterns {
-		if pattern.Match([]byte(cleanedRowsString)) {
-			return len(a.ExpectedOutput), nil
+func (a *MultiLineAssertion) Inspect() string {
+	return fmt.Sprintf("MultiLineAssertion (%q)", a.SingleLineAssertions)
+}
+
+func (a *MultiLineAssertion) Run(screenState [][]string, startRowIndex int) (processedRowCount int, err *AssertionError) {
+	totalProcessedRowCount := 0
+
+	for _, singleLineAssertion := range a.SingleLineAssertions {
+		processedRowCount, err = singleLineAssertion.Run(screenState, startRowIndex+totalProcessedRowCount)
+		if err != nil {
+			return totalProcessedRowCount, err
 		}
+		totalProcessedRowCount += processedRowCount
 	}
-
-	if cleanedRowsString != expectedOutputString {
-		detailedErrorMessage := utils.BuildColoredErrorMessage(expectedOutputString, cleanedRowsString)
-		return 0, &AssertionError{
-			StartRowIndex: startRowIndex,
-			ErrorRowIndex: startRowIndex,
-			Message:       "Output does not match expected value.\n" + detailedErrorMessage,
-		}
-	} else {
-		return len(a.ExpectedOutput), nil
-	}
+	return totalProcessedRowCount, nil
 }
