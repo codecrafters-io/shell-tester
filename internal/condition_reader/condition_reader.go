24888aaeed8cd119aa682ff4e2c7f4cd7be1e8d2
Ryan Gang <ryan-gg@outlook.com>
2024-12-31 01:15:05 +0530

    refactor: add configurable read timeout to ConditionReader
    - Updated NewConditionReader to accept a readTimeout parameter.
    - Modified ReadUntilCondition to utilize the new readTimeout for improved flexibility in reading operations.

--- internal/condition_reader/condition_reader.go
+++ internal/condition_reader/condition_reader.go
@@ -14,16 +14,18 @@ var ErrConditionNotMet = errors.New("condition not met")
 // ConditionReader wraps an io.Reader and provides methods to read until a condition is met
 type ConditionReader struct {
 	asyncReader *async_reader.AsyncReader
+	readTimeout time.Duration
 }
 
-func NewConditionReader(reader io.Reader) ConditionReader {
+func NewConditionReader(reader io.Reader, readTimeout time.Duration) ConditionReader {
 	return ConditionReader{
 		asyncReader: async_reader.New(bufio.NewReader(reader)),
+		readTimeout: readTimeout,
 	}
 }
 
 func (t *ConditionReader) ReadUntilCondition(condition func() bool) error {
-	return t.ReadUntilConditionOrTimeout(condition, 2000*time.Millisecond)
+	return t.ReadUntilConditionOrTimeout(condition, t.readTimeout)
 }
 
 func (t *ConditionReader) ReadUntilConditionOrTimeout(condition func() bool, timeout time.Duration) error {
