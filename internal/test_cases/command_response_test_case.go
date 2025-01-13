bb02ff1e2c6eb3e21f965f95c7e1409f3e4ee656
Ryan Gang <ryan-gg@outlook.com>
2025-01-13 17:38:46 +0530

    refactor: simplify CommandResponseTestCase by delegating to CommandResponseWithCustomReflectionTestCase
    - Refactored the Run method in CommandResponseTestCase to utilize CommandResponseWithCustomReflectionTestCase for command execution and assertion.
    - Removed redundant error handling and logging, enhancing code clarity and maintainability.
    - Streamlined the process of sending commands and verifying outputs, improving the overall testing framework.

--- internal/test_cases/command_response_test_case.go
+++ internal/test_cases/command_response_test_case.go
@@ -1,10 +1,8 @@
 package test_cases
 
 import (
-	"fmt"
 	"regexp"
 
-	"github.com/codecrafters-io/shell-tester/internal/assertions"
 	"github.com/codecrafters-io/shell-tester/internal/logged_shell_asserter"
 	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
 	"github.com/codecrafters-io/tester-utils/logger"
@@ -30,24 +28,11 @@ type CommandResponseTestCase struct {
 }
 
 func (t CommandResponseTestCase) Run(asserter *logged_shell_asserter.LoggedShellAsserter, shell *shell_executable.ShellExecutable, logger *logger.Logger) error {
-	if err := shell.SendCommand(t.Command); err != nil {
-		return fmt.Errorf("Error sending command to shell: %v", err)
-	}
-
-	commandReflection := fmt.Sprintf("$ %s", t.Command)
-	asserter.AddAssertion(assertions.SingleLineAssertion{
-		ExpectedOutput: commandReflection,
-	})
-
-	asserter.AddAssertion(assertions.SingleLineAssertion{
-		ExpectedOutput:   t.ExpectedOutput,
-		FallbackPatterns: t.FallbackPatterns,
-	})
-
-	if err := asserter.AssertWithPrompt(); err != nil {
-		return err
-	}
-
-	logger.Successf("%s", t.SuccessMessage)
-	return nil
+	return CommandResponseWithCustomReflectionTestCase{
+		RawCommand:         t.Command + "\n",
+		ExpectedReflection: t.Command,
+		ExpectedOutput:     t.ExpectedOutput,
+		FallbackPatterns:   t.FallbackPatterns,
+		SuccessMessage:     t.SuccessMessage,
+	}.Run(asserter, shell, logger, true)
 }
