1d2cc8449e18808924dbb48ab42e8a8f5bb1ecaa
Ryan Gang <ryan-gg@outlook.com>
2025-01-14 23:41:41 +0530

    feat: refactor command completion logic in stage_a1.go
    - Consolidated command completion tests into a single helper function for improved readability and maintainability.
    - Introduced a structured input and completion data type to streamline the testing process.
    - Enhanced the command completion test cases by utilizing the new structure, ensuring better organization and clarity in expected outcomes.
    - Removed redundant shell setup code, simplifying the test flow and focusing on completion assertions.

--- internal/stage_a1.go
+++ internal/stage_a1.go
@@ -2,103 +2,58 @@ package internal
 
 import (
 	"fmt"
-	"path"
-	"path/filepath"
 
 	"github.com/codecrafters-io/shell-tester/internal/logged_shell_asserter"
 	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
 	"github.com/codecrafters-io/shell-tester/internal/test_cases"
+	"github.com/codecrafters-io/tester-utils/logger"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
+type inputAndCompletion struct {
+	Input                     string
+	Completion                string
+	CompletionEndsWithNoSpace bool
+}
+
 func testA1(stageHarness *test_case_harness.TestCaseHarness) error {
 	logger := stageHarness.Logger
-	shell := shell_executable.NewShellExecutable(stageHarness)
-	_, err := SetUpCustomCommands(stageHarness, shell, []string{"ls", "cat"})
-	if err != nil {
-		return err
+
+	inputAndCompletion := []inputAndCompletion{
+		{Input: "typ", Completion: "type", CompletionEndsWithNoSpace: true},
+		{Input: "ech", Completion: "echo", CompletionEndsWithNoSpace: false},
+		{Input: "exi", Completion: "exit", CompletionEndsWithNoSpace: false},
 	}
+
+	for _, inputAndCompletion := range inputAndCompletion {
+		err := a1Helper(stageHarness, logger, inputAndCompletion.Input, inputAndCompletion.Completion, inputAndCompletion.CompletionEndsWithNoSpace)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func a1Helper(stageHarness *test_case_harness.TestCaseHarness, logger *logger.Logger, command string, completion string, completionEndsWithNoSpace bool) error {
+	shell := shell_executable.NewShellExecutable(stageHarness)
 	asserter := logged_shell_asserter.NewLoggedShellAsserter(shell)
 
 	if err := asserter.StartShellAndAssertPrompt(); err != nil {
 		return err
 	}
 
-	directory := "xyz"
-	command1 := fmt.Sprintf("cd %s"+"\t"+"\n", directory[:1])
-
-	err = test_cases.CommandWithAttemptedCompletionTestCase{
-		RawCommand:         command1,
-		ExpectedReflection: fmt.Sprintf("cd %s/", directory),
-		ExpectedAutocompletedReflectionHasNoSpace: true,
-		SuccessMessage: fmt.Sprintf("Changed directory to %s", directory),
+	err := test_cases.CommandAutocompleteTestCase{
+		RawCommand:         command,
+		ExpectedReflection: completion,
+		SuccessMessage:     fmt.Sprintf("Received completion for %q", command),
+		ExpectedAutocompletedReflectionHasNoSpace: completionEndsWithNoSpace,
+		SkipPromptAssertion:                       true,
 	}.Run(asserter, shell, logger, false)
 	if err != nil {
 		return err
 	}
 
-	command2 := fmt.Sprintf("cat " + "\t" + "\n")
-	err = test_cases.CommandWithAttemptedCompletionTestCase{
-		RawCommand:         command2,
-		ExpectedReflection: "cat file",
-		ExpectedOutput:     "Hello World!",
-		SuccessMessage:     "Received the contents of xyz/file",
-	}.Run(asserter, shell, logger, false)
-	if err != nil {
-		return err
-	}
-
-	randomDirectory, err := getRandomDirectory(stageHarness)
-	if err != nil {
-		return err
-	}
-	logger.Infof("Random directory: %s", randomDirectory)
-	parentDirectory := path.Dir(randomDirectory)
-	grandParentDirectory := path.Dir(parentDirectory)
-
-	command3 := fmt.Sprintf("cd %s", grandParentDirectory)
-	err = test_cases.CommandReflectionTestCase{
-		Command:        command3,
-		SuccessMessage: fmt.Sprintf("Changed directory to %s", grandParentDirectory),
-	}.Run(asserter, shell, logger, false)
-	if err != nil {
-		return err
-	}
-
-	command4 := "ls"
-	err = test_cases.CommandResponseTestCase{
-		Command:        command4,
-		ExpectedOutput: filepath.Base(parentDirectory),
-		SuccessMessage: fmt.Sprintf("Listed the contents of %s", grandParentDirectory),
-	}.Run(asserter, shell, logger)
-	if err != nil {
-		return err
-	}
-
-	command5 := "cd " + "\t" + "\t" + "\n"
-	autocompleted := fmt.Sprintf("cd %s/%s/", filepath.Base(parentDirectory), filepath.Base(randomDirectory))
-	err = test_cases.CommandWithAttemptedCompletionTestCase{
-		RawCommand:         command5,
-		ExpectedReflection: autocompleted,
-		ExpectedAutocompletedReflectionHasNoSpace: true,
-		SuccessMessage: fmt.Sprintf("Changed directory to %s", filepath.Base(parentDirectory)),
-	}.Run(asserter, shell, logger, false)
-	if err != nil {
-		return err
-	}
-
-	command6 := "ec" + "\t" + "\t"
-	err = test_cases.CommandWithAttemptedCompletionTestCase{
-		RawCommand:         command6,
-		ExpectedReflection: "ec",
-		ExpectedAutocompletedReflectionHasNoSpace: true,
-		ExpectedOutput:      "echo  ecpg",
-		SuccessMessage:      "Received the contents of xyz/file",
-		SkipPromptAssertion: true,
-	}.Run(asserter, shell, logger, false)
-	if err != nil {
-		return err
-	}
-
+	logger.Infof("Tearing down shell")
 	return logAndQuit(asserter, nil)
 }
