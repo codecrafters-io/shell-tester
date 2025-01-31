cf6c2519fca1aa6a9a9501f31bb3e1bfd57ff5e5
Ryan Gang <ryan-gg@outlook.com>
2025-01-31 17:40:39 +0000

    refactor: simplify bell signal reception logic in checkIfBellReceived function

--- internal/assertions/bell_assertion.go
+++ internal/assertions/bell_assertion.go
@@ -1,11 +1,5 @@
 package assertions
 
-import (
-	"time"
-)
-
-const defaultBellTimeout = 100 * time.Millisecond
-
 // BellAssertion asserts that the bell callback function is called
 // by the virtual terminal, this can only happen if the user sends
 // /U0007 to the shell.
@@ -29,15 +23,16 @@ func (a BellAssertion) Run(screenState [][]string, startRowIndex int) (processed
 	}
 }
 
-// checkIfBellReceived waits for a bell signal on the provided channel with a timeout.
-// Returns true if a signal is received, false if the timeout is reached.
+// checkIfBellReceived waits for a bell signal on the provided channel in a non-blocking way.
+// Returns true if a signal is received, false if no signal is received or if the channel is closed.
 func checkIfBellReceived(bellChannel chan bool) bool {
 	select {
-	case <-bellChannel:
-		return true
-	case <-time.After(defaultBellTimeout):
-		// This timeout is for reading the bell signal on the bellChannel
-		// Reading from term & processing happens in x/vt
+	case _, ok := <-bellChannel:
+		if ok {
+			return true
+		}
+		return false
+	default:
 		return false
 	}
 }
