04f24b4619278a773015932437e7ae599fc4a81a
Ryan Gang <ryan-gg@outlook.com>
2025-01-31 15:54:23 +0530

    refactor: improve bell assertion with timeout constant and comments

--- internal/assertions/bell_assertion.go
+++ internal/assertions/bell_assertion.go
@@ -4,7 +4,11 @@ import (
 	"time"
 )
 
-// BellAssertion asserts that ...
+const defaultBellTimeout = 100 * time.Millisecond
+
+// BellAssertion asserts that the bell callback function is called
+// by the virtual terminal, this can only happen if the user sends
+// /U0007 to the shell.
 type BellAssertion struct {
 	// Vt *virtual_terminal.VirtualTerminal
 	BellChannel chan bool
@@ -15,22 +19,25 @@ func (a BellAssertion) Inspect() string {
 }
 
 func (a BellAssertion) Run(screenState [][]string, startRowIndex int) (processedRowCount int, err *AssertionError) {
-	if !checkIfBellReceived(a.BellChannel) {
-		return 0, &AssertionError{
-			StartRowIndex: startRowIndex,
-			ErrorRowIndex: startRowIndex,
-			Message:       "Expected bell to ring, but it didn't",
-		}
-	} else {
+	if checkIfBellReceived(a.BellChannel) {
 		return 0, nil
+	}
+	return 0, &AssertionError{
+		StartRowIndex: startRowIndex,
+		ErrorRowIndex: startRowIndex,
+		Message:       "Expected bell to ring, but it didn't",
 	}
 }
 
+// checkIfBellReceived waits for a bell signal on the provided channel with a timeout.
+// Returns true if a signal is received, false if the timeout is reached.
 func checkIfBellReceived(bellChannel chan bool) bool {
 	select {
 	case <-bellChannel:
 		return true
-	case <-time.After(100 * time.Millisecond): // Add reasonable timeout
+	case <-time.After(defaultBellTimeout):
+		// This timeout is for reading the bell signal on the bellChannel
+		// Reading from term & processing happens in x/vt
 		return false
 	}
 }
