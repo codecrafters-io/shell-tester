e3e0ab650698b35ef8862616aa79ea6060c0011f
Ryan Gang <ryan-gg@outlook.com>
2025-04-23 15:51:07 +0530

    refactor: remove unnecessary logging and simplify error handling in signature printer build process

--- internal/custom_executable/build/signaturePrinter.go
+++ internal/custom_executable/build/signaturePrinter.go
@@ -2,7 +2,6 @@ package custom_executable
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"path"
@@ -21,10 +20,8 @@ func CreateSignaturePrinterExecutable(randomString, outputPath string) error {
 
 	ldflags := fmt.Sprintf("-X '%s=%s'", secretCodeVariablePath, randomString)
 
-	name := "signature_printer"
 	sourcePath := path.Join(os.Getenv("TESTER_DIR"), "internal", "custom_executable", "signature_printer", "main.go")
 
-	fmt.Printf("Building %s for %s/%s -> %s\n", name, goos, goarch, outputPath)
 	cmd := exec.Command("go", "build", "-o", outputPath, "-ldflags", ldflags, sourcePath)
 	cmd.Env = append(os.Environ(),
 		fmt.Sprintf("GOOS=%s", goos),
@@ -32,10 +29,7 @@ func CreateSignaturePrinterExecutable(randomString, outputPath string) error {
 	)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Printf("Error building %s for %s/%s: %v\nOutput:\n%s", name, goos, goarch, err, string(output))
-		panic("CodeCrafters Internal Error: failed to build signature printer executable for " + goos + "/" + goarch)
-	} else if len(output) > 0 {
-		fmt.Printf("Output for %s %s/%s:\n%s\n", name, goos, goarch, string(output))
+		panic("CodeCrafters Internal Error: failed to build signature printer executable for " + goos + "/" + goarch + "\n" + string(output))
 	}
 
 	return nil
