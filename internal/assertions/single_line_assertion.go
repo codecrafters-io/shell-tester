707257e5e9d0bff2da1e9181b4d8902ecf4e8a1d
Ryan Gang <ryan-gg@outlook.com>
2025-04-28 22:10:06 +0530

    fix: add panic for missing ExpectedOutput and FallbackPatterns in SingleLineAssertion

--- internal/assertions/single_line_assertion.go
+++ internal/assertions/single_line_assertion.go
@@ -28,6 +28,9 @@ func (a SingleLineAssertion) Inspect() string {
 
 func (a SingleLineAssertion) Run(screenState [][]string, startRowIndex int) (processedRowCount int, err *AssertionError) {
 	if a.ExpectedOutput == "" {
+		if len(a.FallbackPatterns) == 0 {
+			panic("CodeCrafters Internal Error: ExpectedOutput must be provided")
+		}
 		return 0, nil
 	}
 
