e7110a170d5e6b94b1256cccc414d431a8d8108f
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-06-11 11:08:23 -0700

    Add error handling and separate build/run commands in shell test scripts

--- local_testing/run_bash_shell_tests.sh
+++ local_testing/run_bash_shell_tests.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -e
+
 # Ensure we're in the correct directory
 cd "$(dirname "$0")/.."
 
 # Build and run
-docker build -t shell-tester -f local_testing/bash_shell.Dockerfile . && docker run --rm -it shell-tester make test_bash
+docker build -t shell-tester -f local_testing/bash_shell.Dockerfile .
+docker run --rm -it shell-tester make test_bash
