df94919f25a2cc5ca46168a94b87310e36c20660
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-06-19 15:31:26 +0800

    Update signature printer executables and enhance argument handling in tests
    - Updated binary files for signature printer executables on Darwin (amd64, arm64) and Linux (amd64, arm64).
    - Refactored `stage8.go` to generate multiple random executable names and codes for improved testing.
    - Enhanced argument handling in `main.go` to print all provided arguments dynamically.
    - Updated test fixtures to reflect changes in executable names and paths.

--- internal/custom_executable/signature_printer/main.go
+++ internal/custom_executable/signature_printer/main.go
@@ -8,15 +8,12 @@ import (
 func main() {
 	secretCode := "<<RANDOM>>"
 
-	var randomCode string
-	if len(os.Args) > 1 {
-		randomCode = os.Args[1]
-	} else {
-		randomCode = "<no argument provided>"
-	}
-
 	fmt.Printf("Program was passed %d args (including program name).\n", len(os.Args))
 	fmt.Printf("Arg #0 (program name): %s\n", os.Args[0])
-	fmt.Printf("Arg #1: %s\n", randomCode)
+
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("Arg #%d: %s\n", i, os.Args[i])
+	}
+
 	fmt.Printf("Program Signature: %s\n", secretCode)
 }
--- internal/stage8.go
+++ internal/stage8.go
@@ -16,37 +16,65 @@ import (
 func testRun(stageHarness *test_case_harness.TestCaseHarness) error {
 	logger := stageHarness.Logger
 	shell := shell_executable.NewShellExecutable(stageHarness)
-	randomCode := getRandomString()
-	randomName := getRandomName()
-	randomExecutableName := "custom_exe_" + strconv.Itoa(random.RandomInt(1000, 9999))
+
+	randomCode1 := getRandomString()
+	randomCode2 := getRandomString()
+
+	randomName1 := getRandomName()
+	randomName2 := getRandomName()
+	randomName3 := getRandomName()
+
+	randomExecutableName1 := "custom_exe_" + strconv.Itoa(random.RandomInt(1000, 9999))
+	randomExecutableName2 := "custom_exe_" + strconv.Itoa(random.RandomInt(1000, 9999))
+
 	_, err := SetUpCustomCommands(stageHarness, shell, []CommandDetails{
-		{CommandType: "signature_printer", CommandName: randomExecutableName, CommandMetadata: randomCode},
+		{CommandType: "signature_printer", CommandName: randomExecutableName1, CommandMetadata: randomCode1},
+		{CommandType: "signature_printer", CommandName: randomExecutableName2, CommandMetadata: randomCode2},
 	}, true)
 	if err != nil {
 		return err
 	}
-	logAvailableExecutables(logger, []string{randomExecutableName})
+	logAvailableExecutables(logger, []string{randomExecutableName1, randomExecutableName2})
 	asserter := logged_shell_asserter.NewLoggedShellAsserter(shell)
 
 	if err := asserter.StartShellAndAssertPrompt(true); err != nil {
 		return err
 	}
 
-	command := []string{
-		randomExecutableName, randomName,
+	command1 := []string{
+		randomExecutableName1, randomName1,
 	}
 
 	testCase := test_cases.CommandWithMultilineResponseTestCase{
-		Command: strings.Join(command, " "),
+		Command: strings.Join(command1, " "),
 		MultiLineAssertion: assertions.NewMultiLineAssertion([]string{
-			fmt.Sprintf("Program was passed %d args (including program name).", len(command)),
-			fmt.Sprintf("Arg #0 (program name): %s", command[0]),
-			fmt.Sprintf("Arg #1: %s", command[1]),
-			fmt.Sprintf("Program Signature: %s", randomCode),
+			fmt.Sprintf("Program was passed %d args (including program name).", len(command1)),
+			fmt.Sprintf("Arg #0 (program name): %s", command1[0]),
+			fmt.Sprintf("Arg #1: %s", command1[1]),
+			fmt.Sprintf("Program Signature: %s", randomCode1),
 		}),
 		SuccessMessage: "✓ Received expected response",
 	}
 	if err := testCase.Run(asserter, shell, logger); err != nil {
+		return err
+	}
+
+	command2 := []string{
+		randomExecutableName2, randomName2, randomName3,
+	}
+
+	testCase2 := test_cases.CommandWithMultilineResponseTestCase{
+		Command: strings.Join(command2, " "),
+		MultiLineAssertion: assertions.NewMultiLineAssertion([]string{
+			fmt.Sprintf("Program was passed %d args (including program name).", len(command2)),
+			fmt.Sprintf("Arg #0 (program name): %s", command2[0]),
+			fmt.Sprintf("Arg #1: %s", command2[1]),
+			fmt.Sprintf("Arg #2: %s", command2[2]),
+			fmt.Sprintf("Program Signature: %s", randomCode2),
+		}),
+		SuccessMessage: "✓ Received expected response",
+	}
+	if err := testCase2.Run(asserter, shell, logger); err != nil {
 		return err
 	}
 
