c5b0087b8001c4db3b586c1caf517d49329b5bfa
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-06-27 17:21:45 +0800

    Refactor isTestingTesterUsingBusyboxOnAlpine function to accept stage harness
    - Updated the `isTestingTesterUsingBusyboxOnAlpine` function to take a `stageHarness` parameter, improving its integration with the test case harness.
    - Removed the previous process name checks and streamlined the detection logic to focus on the executable path, enhancing clarity and efficiency.
    - Cleaned up the `your_shell.sh` script by removing unnecessary whitespace.

--- internal/utils_sys.go
+++ internal/utils_sys.go
@@ -1,57 +1,18 @@
 package internal
 
 import (
-	"fmt"
 	"os"
-	"strconv"
 	"strings"
+
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
-func getCommandName(pid int) string {
-	commPath := fmt.Sprintf("/proc/%d/comm", pid)
-	if commData, err := os.ReadFile(commPath); err == nil {
-		return strings.TrimSpace(string(commData))
-	}
-	return "unknown"
-}
-
-func getParentPid(pid int) int {
-	if statData, err := os.ReadFile(fmt.Sprintf("/proc/%d/stat", pid)); err == nil {
-		fields := strings.Fields(string(statData))
-		if len(fields) > 3 {
-			if parentPid, err := strconv.Atoi(fields[3]); err == nil {
-				return parentPid
-			}
-		}
-	}
-	return 0
-}
-
-func isTestingTesterUsingBusyboxOnAlpine() bool {
+func isTestingTesterUsingBusyboxOnAlpine(stageHarness *test_case_harness.TestCaseHarness) bool {
+	path := stageHarness.Executable.Path
+	isTestingTesterUsingBusybox := strings.HasSuffix(path, "internal/test_helpers/ash/your_shell.sh")
 
 	_, err := os.Stat("/etc/alpine-release")
 	isOnAlpine := err == nil
 
-	if !isOnAlpine {
-		return false
-	}
-
-	pid := os.Getppid()
-	fmt.Println("⛳ ppid:", pid)
-	current_name := getCommandName(pid)
-
-	parent_pid := getParentPid(pid)
-	parent_name := getCommandName(parent_pid)
-
-	great_parent_pid := getParentPid(parent_pid)
-	great_parent_name := getCommandName(great_parent_pid)
-
-	fmt.Println("⛳ Current:", current_name)
-	fmt.Println("⛳ Parent:", parent_name)
-	fmt.Println("⛳ Great-Parent:", great_parent_name)
-
-	isTestingTesterUsingBusybox := current_name == "go" && parent_name == "make" && great_parent_name == "ash"
-
-	return true
-	return isTestingTesterUsingBusybox
+	return isTestingTesterUsingBusybox && isOnAlpine
 }
