0f0d23b20aa8495c43c027e315b1721987fd9eaa
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-07-01 03:11:12 +0800

    Refactor isTestingTesterUsingBusyboxOnAlpine function to utilize a list of Busybox shell paths
    - Introduced a new constant, BUSYBOX_SHELL_PATHS, to store multiple shell script paths.
    - Updated the isTestingTesterUsingBusyboxOnAlpine function to check against this list, improving flexibility and maintainability of the shell detection logic.

--- internal/utils_sys.go
+++ internal/utils_sys.go
@@ -7,9 +7,20 @@ import (
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
+var BUSYBOX_SHELL_PATHS = []string{
+	"internal/test_helpers/ash/your_shell.sh",
+	"internal/test_helpers/dash/your_shell.sh",
+}
+
 func isTestingTesterUsingBusyboxOnAlpine(stageHarness *test_case_harness.TestCaseHarness) bool {
 	path := stageHarness.Executable.Path
-	isTestingTesterUsingBusybox := strings.HasSuffix(path, "internal/test_helpers/ash/your_shell.sh")
+	isTestingTesterUsingBusybox := false
+	for _, busyboxShellPath := range BUSYBOX_SHELL_PATHS {
+		if strings.HasSuffix(path, busyboxShellPath) {
+			isTestingTesterUsingBusybox = true
+			break
+		}
+	}
 
 	_, err := os.Stat("/etc/alpine-release")
 	isOnAlpine := err == nil
