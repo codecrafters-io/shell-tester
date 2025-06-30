5e20061ffa391f5ce095caf14777eef2ccb02e02
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-07-01 03:17:44 +0800

    Refactor isTestingTesterUsingBusyboxOnAlpine function to simplify shell path checks
    - Streamlined the logic in the `isTestingTesterUsingBusyboxOnAlpine` function by directly checking the executable path against the first two entries in the BUSYBOX_SHELL_PATHS constant.
    - Removed the loop for improved readability and performance.

--- internal/utils_sys.go
+++ internal/utils_sys.go
@@ -7,20 +7,12 @@ import (
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
-var BUSYBOX_SHELL_PATHS = []string{
-	"internal/test_helpers/ash/your_shell.sh",
-	"internal/test_helpers/dash/your_shell.sh",
-}
+const ASH_PATH = "internal/test_helpers/ash/your_shell.sh"
+const DASH_PATH = "internal/test_helpers/dash/your_shell.sh"
 
 func isTestingTesterUsingBusyboxOnAlpine(stageHarness *test_case_harness.TestCaseHarness) bool {
 	path := stageHarness.Executable.Path
-	isTestingTesterUsingBusybox := false
-	for _, busyboxShellPath := range BUSYBOX_SHELL_PATHS {
-		if strings.HasSuffix(path, busyboxShellPath) {
-			isTestingTesterUsingBusybox = true
-			break
-		}
-	}
+	isTestingTesterUsingBusybox := strings.HasSuffix(path, ASH_PATH) || strings.HasSuffix(path, DASH_PATH)
 
 	_, err := os.Stat("/etc/alpine-release")
 	isOnAlpine := err == nil
