ab9d73278ce69cd249128c57a497b177b7c27037
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2026-03-04 08:39:49 +0545

    Add background output test case in stage bg2

--- internal/stage_bg2.go
+++ internal/stage_bg2.go
@@ -1,9 +1,13 @@
 package internal
 
 import (
+	"fmt"
+
+	"al.essio.dev/pkg/shellescape"
 	"github.com/codecrafters-io/shell-tester/internal/logged_shell_asserter"
 	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
 	"github.com/codecrafters-io/shell-tester/internal/test_cases"
+	"github.com/codecrafters-io/tester-utils/random"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
@@ -16,13 +20,37 @@ func testBG2(stageHarness *test_case_harness.TestCaseHarness) error {
 		return err
 	}
 
+	// Create a named pipe
+	fifoPath := fmt.Sprintf("/tmp/%s-%d", random.RandomWord(), random.RandomInt(1, 100))
+	if err := CreateRandomFIFOWithTeardown(stageHarness, fifoPath, 0644); err != nil {
+		return err
+	}
+
+	grepPattern := random.RandomWord()
+	bgGrepCommand := fmt.Sprintf("grep %s %s", grepPattern, fifoPath)
+
 	testCase := test_cases.BackgroundCommandResponseTestCase{
-		Command:           "sleep 100",
+		Command:           bgGrepCommand,
 		SuccessMessage:    "✓ Received next prompt",
 		ExpectedJobNumber: 1,
 	}
 
 	if err := testCase.Run(asserter, shell, logger); err != nil {
+		return err
+	}
+
+	// Write to the fifo
+	if err := WriteToFile(stageHarness, fifoPath, grepPattern); err != nil {
+		return err
+	}
+
+	// Assert background command output
+	backgroundCommandOutputTestCase := test_cases.BackgroundCommandOutputOnlyTestCase{
+		ExpectedOutputLines: []string{grepPattern},
+		SuccessMessage:      fmt.Sprintf("✓ Output of %s found in the next prompt line", shellescape.Quote(bgGrepCommand)),
+	}
+
+	if err := backgroundCommandOutputTestCase.Run(asserter, shell, logger); err != nil {
 		return err
 	}
 
