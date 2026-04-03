9e461da663abed54717b12fd0a30db1bcfd95f77
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2026-04-03 14:02:13 +0545

    Add stage 10

--- internal/stage_pa10.go
+++ internal/stage_pa10.go
@@ -1,7 +1,53 @@
 package internal
 
-import "github.com/codecrafters-io/tester-utils/test_case_harness"
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/codecrafters-io/shell-tester/internal/logged_shell_asserter"
+	"github.com/codecrafters-io/shell-tester/internal/shell_executable"
+	"github.com/codecrafters-io/shell-tester/internal/test_cases"
+	"github.com/codecrafters-io/tester-utils/random"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
 
 func testPA10(stageHarness *test_case_harness.TestCaseHarness) error {
-	return nil
+	logger := stageHarness.Logger
+	shell := shell_executable.NewShellExecutable(stageHarness)
+	asserter := logged_shell_asserter.NewLoggedShellAsserter(shell)
+
+	if err := asserter.StartShellAndAssertPrompt(true); err != nil {
+		return err
+	}
+
+	commandName := "git"
+	completerFileBasename := fmt.Sprintf("%s.py", random.RandomWord())
+	completerPath := filepath.Join("/tmp", completerFileBasename)
+
+	registerCmd := fmt.Sprintf("complete  -C  '%s'  %s", completerPath, commandName)
+	if err := (test_cases.CommandWithNoResponseTestCase{
+		Command:        registerCmd,
+		SuccessMessage: "✓ No output found",
+	}).Run(asserter, shell, logger, false); err != nil {
+		return err
+	}
+
+	unregisterCmd := fmt.Sprintf("complete -r %s", commandName)
+	if err := (test_cases.CommandWithNoResponseTestCase{
+		Command:        unregisterCmd,
+		SuccessMessage: "✓ No output from complete -r",
+	}).Run(asserter, shell, logger, false); err != nil {
+		return err
+	}
+
+	if err := (test_cases.AutocompleteTestCase{
+		RawInput:            commandName + " ",
+		ExpectedCompletion:  commandName + " ",
+		CheckForBell:        true,
+		SkipPromptAssertion: true,
+	}).Run(asserter, shell, logger); err != nil {
+		return err
+	}
+
+	return logAndQuit(asserter, nil)
 }
