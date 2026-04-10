c7841e12e030f5da5efe35d99d847c885e43eaeb
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2026-04-10 16:30:18 +0545

    Remove bash and zsh path from IsTestingTesterUsingBusyboxOnAlpine

--- internal/utils/utils_sys.go
+++ internal/utils/utils_sys.go
@@ -7,13 +7,10 @@ import (
 
 const ASH_PATH = "internal/test_helpers/ash/your_shell.sh"
 const DASH_PATH = "internal/test_helpers/dash/your_shell.sh"
-const BASH_PATH = "internal/test_helpers/bash/your_shell.sh"
-const ZSH_PATH = "internal/test_helpers/zsh/your_shell.sh"
 
 func IsTestingTesterUsingBusyboxOnAlpine(shellPath string) bool {
 	isTestingTesterUsingBusybox :=
-		strings.HasSuffix(shellPath, ASH_PATH) || strings.HasSuffix(shellPath, DASH_PATH) ||
-			strings.HasSuffix(shellPath, BASH_PATH) || strings.HasSuffix(shellPath, ZSH_PATH)
+		strings.HasSuffix(shellPath, ASH_PATH) || strings.HasSuffix(shellPath, DASH_PATH)
 
 	_, err := os.Stat("/etc/alpine-release")
 	isOnAlpine := err == nil
