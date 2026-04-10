cf7f51197ddb5e7256710821261d83f38cee2723
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2026-04-10 15:26:24 +0545

    Update IsTestingTester

--- internal/utils/utils_sys.go
+++ internal/utils/utils_sys.go
@@ -7,9 +7,12 @@ import (
 
 const ASH_PATH = "internal/test_helpers/ash/your_shell.sh"
 const DASH_PATH = "internal/test_helpers/dash/your_shell.sh"
+const BASH_PATH = "internal/test_helpers/bash/your_shell.sh"
 
 func IsTestingTester(executablePath string) bool {
-	return strings.HasSuffix(executablePath, ASH_PATH) || strings.HasSuffix(executablePath, DASH_PATH)
+	return (strings.HasSuffix(executablePath, ASH_PATH) ||
+		strings.HasSuffix(executablePath, DASH_PATH) ||
+		strings.HasSuffix(executablePath, BASH_PATH))
 }
 
 func IsTestingTesterUsingBusyboxOnAlpine(executablePath string) bool {
